a5f33a381364cd74f886e9ed45da50bd580bbd3a
Jason Y. Lee <jylee@blizzard.net>
2018-05-08 16:05:00 -0700

    Finalize command line

--- create_centos_ami.py
+++ create_centos_ami.py
@@ -12,7 +12,7 @@
 import argparse
 import datetime
 from hashlib import sha256
-#import json
+import json
 import logging
 import os
 import re
@@ -139,8 +139,10 @@ class CentOSVagrantBox(object):
         """Returns the temp directory"""
         return self.tmpdir
 
-    def get_vmdkfile(self):
+    def get_vmdkfile(self, fullpath=False):
         """Return vmdk filename"""
+        if fullpath:
+            return os.path.join(self.tmpdir, self.vmdkfile)
         return self.vmdkfile
 
     def get_description_for_aws(self):
@@ -169,6 +171,7 @@ class AWSConvertVMDK2AMI(object):
             source_region,
             destination_regions=None,
             bucket=None,
+            rolename=None,
             tags=None,
             verification=False,
             aws_access_key=None,
@@ -192,8 +195,12 @@ class AWSConvertVMDK2AMI(object):
         }
         self.description = description
         self.bucketname = bucket
+        self.rolename = rolename
         self.tags = tags
-        self.temporary = bucket is None
+        self.temporary = {
+            'Bucket': bucket is None,
+            'Rolename': rolename is None
+        }
         if aws_profile is not None:
             if aws_access_key is None and aws_secret_key is None:
                 self.aws_credentials['Session'] = boto3.Session(
@@ -253,34 +260,44 @@ class AWSConvertVMDK2AMI(object):
         client = session.client(
             's3',
             config=Config(signature_version='s3v4'))
-        resource = session.resource('s3')
 
         if self.bucketname is not None:
             try:
                 response = client.head_bucket(Bucket=self.bucketname)
-                self.temporary = response['ResponseMetadata'][
+                self.temporary['Bucket'] = response['ResponseMetadata'][
                     'HTTPStatusCode'] == 200
             except ClientError:
                 self.bucketname = None
 
         if self.bucketname is None:
-            self.bucketname = 'CentOS.AMI.{}.{}'.format(
+            self.bucketname = 'centos.ami.{}.{}'.format(
                 self.filename,
                 datetime.datetime.utcnow().strftime('%Y%m%d.%H%M%S'))
+            self.bucketname = self.bucketname.lower().replace('_', '-')
             if self.source_region != 'us-east-1':
-                bucket = resource.create_bucket(
-                    Bucket=self.bucketname,
-                    ACL='private',
-                    CreateBucketConfiguration={
-                        'LocationConstraint': self.source_region})
+                try:
+                    client.create_bucket(
+                        Bucket=self.bucketname,
+                        ACL='private',
+                        CreateBucketConfiguration={
+                            'LocationConstraint': self.source_region})
+                except ClientError as exception:
+                    raise Exception(str(
+                        'Bucket Name: {}').format(
+                            self.bucketname)) from exception
             else:
-                bucket = resource.create_bucket(
-                    Bucket=self.bucketname,
-                    ACL='private')
+                try:
+                    client.create_bucket(
+                        Bucket=self.bucketname,
+                        ACL='private')
+                except ClientError as exception:
+                    raise Exception(str(
+                        'Bucket Name: {}').format(
+                            self.bucketname)) from exception
 
             if self.tags is not None:
                 client.put_bucket_tagging(
-                    Bucket=bucket.name,
+                    Bucket=self.bucketname,
                     Tagging={
                         'TagSet': self.tags
                     }
@@ -293,7 +310,7 @@ class AWSConvertVMDK2AMI(object):
                     'Rules': [
                         {
                             'ApplyServerSideEncryptionByDefault': {
-                                'SEEAlgorithm': 'AES256'
+                                'SSEAlgorithm': 'AES256'
                             }
                         }
                     ]
@@ -316,9 +333,10 @@ class AWSConvertVMDK2AMI(object):
         self.check_s3_bucket(
             s3_access_key=s3_access_key,
             s3_secret_key=s3_secret_key)
+        resource = session.resource('s3')
 
         if self.verification:
-            sha256sum = calculate_sha256_sum(self.filename)
+            sha256sum = calculate_sha256_sum(self.fullpath)
 
             extra_args = {'Metadata': {'x-amz-content-sha256': sha256sum}}
 
@@ -330,7 +348,7 @@ class AWSConvertVMDK2AMI(object):
                 extra_args)
 
             # Re-download the file to verify
-            s3_object = client.Object(
+            s3_object = resource.Object(
                 self.bucketname,
                 self.filename)
             s3_object.download_file(self.fullpath + '.verify')
@@ -349,6 +367,78 @@ class AWSConvertVMDK2AMI(object):
                 self.filename,
                 extra_args)
 
+    def check_or_create_iam_role(self):
+        """Checks if specified rolename exists"""
+
+        iam = self.aws_credentials['Session'].client('iam')
+        if self.rolename is not None:
+            try:
+                iam.get_role(RoleName=self.rolename)
+            except iam.exceptions.NoSuchEntityException:
+                self.rolename = None
+
+        if self.rolename is None:
+            self.rolename = 'vmimport-{}'.format(self.bucketname)
+            iam.create_role(
+                RoleName=self.rolename,
+                AssumeRolePolicyDocument=json.dumps({
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Principal": {
+                                "Service": "vmie.amazonaws.com"
+                            },
+                            "Action": "sts:AssumeRole",
+                            "Condition": {
+                                "StringEquals": {
+                                    "sts:Externalid": "vmimport"
+                                }
+                            }
+                        }
+                    ]
+                }),
+                Description='Role for vmimport for {}'.format(
+                    self.description),
+            )
+            iam.put_role_policy(
+                RoleName=self.rolename,
+                PolicyName=self.rolename,
+                PolicyDocument=json.dumps({
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": [
+                                "s3:GetBucketLocation",
+                                "s3:GetObject",
+                                "s3:ListBucket"
+                            ],
+                            "Resource": [
+                                "arn:aws:s3:::{}".format(self.bucketname),
+                                "arn:aws:s3:::{}/*".format(self.bucketname)
+                            ]
+                        },
+                        {
+                            "Effect": "Allow",
+                            "Action": [
+                                "ec2:ModifySnapshotAttribute",
+                                "ec2:CopySnapshot",
+                                "ec2:RegisterImage",
+                                "ec2:Describe*"
+                            ],
+                            "Resource": "*"
+                        }
+                    ]
+                })
+            )
+
+    def cleanup_role(self, force=False):
+        """Deletes the temporary created role"""
+        if self.temporary['Rolename'] or force:
+            iam = self.aws_credentials['Session'].client('iam')
+            iam.delete_role(RoleName=self.rolename)
+
     def export_ami(self, alt_access_key=None, alt_secret_key=None):
         """Exports the AMI from the uploaded S3 VMDK"""
         session = self.aws_credentials['Session']
@@ -360,6 +450,8 @@ class AWSConvertVMDK2AMI(object):
 
         if self.bucketname is None:
             self.upload_to_s3(alt_access_key, alt_secret_key)
+
+        self.check_or_create_iam_role()
 
         ec2 = session.client('ec2', region_name=self.source_region)
         response = ec2.import_image(
@@ -376,7 +468,8 @@ class AWSConvertVMDK2AMI(object):
             ],
             Hypervisor='xen',
             LicenseType='AWS',
-            Platform='Linux'
+            Platform='Linux',
+            RoleName=self.rolename
         )
         temporary_ami = response['ImageId']
 
@@ -397,12 +490,14 @@ class AWSConvertVMDK2AMI(object):
         self.cleanup()
 
     def cleanup(self, force=False):
-        """Clean up the uploaded file and temporary bucket"""
+        """Clean up any temporary created resources"""
         client = self.aws_credentials['Session'].client('s3')
         client.delete_object(
             Bucket=self.bucketname,
             Key=self.filename)
-        if self.temporary:
+        if self.temporary['Bucket']:
+            # If the bucket is forcibly deleted,
+            # remove all objects first then delete the bucket
             if force:
                 while True:
                     response = client.list_objects_v2(Bucket=self.bucketname)
@@ -419,12 +514,11 @@ class AWSConvertVMDK2AMI(object):
 
             client.delete_bucket(
                 Bucket=self.bucketname)
+        self.cleanup_role(force)
 
 def make_opt_parser():
     """Parse the options from command line"""
     parser = argparse.ArgumentParser(description='Import virtualbox vagrant box as AWS AMI')
-    parser.add_argument('--region', default='cn-north-1')
-    parser.add_argument('--s3bucket', help='s3bucket', default=None)
     parser.add_argument('--version',
                         required=False,
                         help='The CentOS version that you want to use',
@@ -437,6 +531,41 @@ def make_opt_parser():
                         default='./tmpdir',
                         help="Temporary dir WARNING: "
                         "it will be cleaned up before and after operation",
+                       )
+    parser.add_argument('--bucket',
+                        default=None,
+                        help="Specify a bucket to use,"
+                        "or one will be generated for you"
+                       )
+    parser.add_argument('--rolename',
+                        default=None,
+                        help="Rolename for VM Import process"
+                       )
+    parser.add_argument('--aws_access_key_id',
+                        default=None,
+                        help="Specify an AWS Access Key ID to use"
+                       )
+    parser.add_argument('--aws_secret_access_key_id',
+                        default=None,
+                        help="Specify an AWS Secret Key ID to use"
+                       )
+    parser.add_argument('--aws_profile_name',
+                        default=None,
+                        help="Specify an AWS Profile Name"
+                       )
+    parser.add_argument('--source_region',
+                        required=True,
+                        default='us-east-1',
+                        help="Source Region to put the first AMI in"
+                       )
+    parser.add_argument('--destination_regions',
+                        required=True,
+                        help="Destination Regions to copy AMI to,"
+                        " in comma delimited format"
+                       )
+    parser.add_argument('--tags',
+                        help="Tags in JSON format, passed as a list of "
+                        "{\"Key\": \"Key-name\", \"Value\": \"Some-value\"}"
                        )
     parser.add_argument('--verbose',
                         required=False,
@@ -455,8 +584,34 @@ def main(opts):
         centos_version=opts.version,
         revision=opts.revision,
         tmpdir=opts.tempdir)
-    box.download()
-    box.convert_to_vmdk()
+    try:
+        box.download()
+        box.convert_to_vmdk()
+    except Exception:
+        box.cleanup()
+        raise
+
+    awsconvert = AWSConvertVMDK2AMI(
+        filename=box.get_vmdkfile(fullpath=True),
+        description=box.get_description_for_aws(),
+        source_region=opts.source_region,
+        destination_regions=opts.destination_regions.split(','),
+        bucket=opts.bucket,
+        rolename=opts.rolename,
+        tags=json.loads(opts.tags),
+        verification=re.match('^cn-', opts.source_region) is not None,
+        aws_access_key=opts.aws_access_key_id,
+        aws_secret_key=opts.aws_secret_access_key_id,
+        aws_profile=opts.aws_profile_name)
+    try:
+        awsconvert.export_ami()
+    except Exception:
+        awsconvert.cleanup()
+        raise
+
+    box.cleanup()
+    awsconvert.cleanup()
+
     sys.exit(0)
 
 if __name__ == '__main__':
